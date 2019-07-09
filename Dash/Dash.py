b79d79f1db47b1bff66f57b9483a99063b8e0c42
Mosali Harsha Vardhan Reddy <harshavardhanm03@gmail.com>
2019-07-09 06:48:30 -0400

    Add files via upload

--- Dash/Dash.py
+++ Dash/Dash.py
@@ -25,7 +25,9 @@ from nltk.corpus import stopwords
 
 
 #from plotly_wordcloud import plotly_wordcloud as pwc
-#import ConfigParser
+
+
+
 
 
 
@@ -33,19 +35,11 @@ from nltk.corpus import stopwords
 app = dash.Dash()
 import pandas as pd
 
-
+#Create a connection to redshift
 connection = psycopg2.connect(user = "harsha",password = "Harsha9652#",host = "amazon.cpe3nnjoxfg1.us-east-1.redshift.amazonaws.com",port ="5439",database = "amazon")
 cur = connection.cursor(cursor_factory=RealDictCursor)
-"""cur.execute(select 
-            product_title_no_punc as product_name,
-            review_date as review_date,
-            avg(avg_star_rating_daily) as avg_star_rating,
-			total_votes_in_day as total_votes,
-			reviews_no_punc as review_text
-            from product_trends
-			group by()
-			)"""
-			
+
+#Select the date using redshift query
 cur.execute("""select 
 			product_title_no_punc as product_name,
 			count(product_title_no_punc),
@@ -58,21 +52,12 @@ cur.execute("""select
 			order by review_date"""
 			)		
 			
+			
 df=pd.DataFrame(cur.fetchall())
 products=df['product_name'].unique()
 
-cur.execute("""select
-			product_title_no_punc as product_name,
-			reviews_no_punc as review_text,
-            avg_star_rating_daily as avg_rating 
-            from product_trends2
-			""")
 
-df2=pd.DataFrame(cur.fetchall())
-
-df2.columns
-
-
+#CSS and HTML STYLING and LAYOUT DEVELOPING
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -114,14 +99,16 @@ app.layout = html.Div(
         ], className="six columns"),
 
         html.Div([
-            html.H3('Reviews Trend'),
+            html.H3('Sentiment Trend'),
             dcc.Graph(id='g2')
         ], className="six columns"),
     ], className="row")
 						
 
 		])
+
 		
+#Call back to figure 1
 @app.callback(
 	Output(component_id='g1', component_property='figure'),
 	[Input(component_id='product_id',component_property='value')
@@ -149,40 +136,6 @@ def update_figure(product):
     }
 	
 
-
-product_words=[word for word in df2['product_name'].unique()]
-for i in range(0,len(product_words)):
-    product_words.append(product_words[i].split(' '))
-
-
-
-stop_words = list(stopwords.words('english'))
-stop_words.extend(product_words) 
-"""
-def clean_reviews(df2,product):
-    filtered_df=df2[df2['product_name']==product]['review_text']
-    text="".join(filtered_df)
-    tokens = nltk.word_tokenize(text)
-    tokens=[w for w in tokens if not w in stop_words] 
-    return (tokens)
-tokens_list=clean_reviews(df2,product)
-
-
-
-count_words=Counter(tokens_list)
-most_common_words=count_words.most_common(40)
-
-
-tokens_x=[]
-tokens_y=[]
-for i in range(0,len(most_common_words)):
-    tokens_x.append(most_common_words[i][0])
-    tokens_y.append(most_common_words[i][1])
-
-
-print(x,y)"""
-
-
 @app.callback(
 	Output(component_id='g2', component_property='figure'),
 	[Input(component_id='product_id',component_property='value')
@@ -191,37 +144,23 @@ print(x,y)"""
 	]
 			)	
 			
-def update_figure(product):
-	filtered_df=df2[df2['product_name']==product]['review_text']
-	text="".join(filtered_df)
-	tokens = nltk.word_tokenize(text)
-	tokens=[w for w in tokens if not w in stop_words] 
-	count_words=Counter(tokens)
-	most_common_words=count_words.most_common(40)
-
-
-	tokens_x=[]
-	tokens_y=[]
-	for i in range(0,len(most_common_words)):
-		tokens_x.append(most_common_words[i][0])
-		tokens_y.append(most_common_words[i][1])
-
+def update_figure2(product):
+	filtered_df=df[df['product_name']==product]
+	#x=filtered_df['review_date'].sort_values()
+	#y=filtered_df[' _star_rating_daily']
 	return  {
 
         'data': [{
 
-            'x': tokens_x,
+            'x': filtered_df['review_date'].sort_values(),
 
-            'y': tokens_y,
-			
-			'type': 'bar'
+            'y': filtered_df['helpful_votes_week']
 
         }],
 
         'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
 
     }
-	
 
 
 if __name__ == '__main__':
